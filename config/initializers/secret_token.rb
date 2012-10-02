89ef339d57bd0a51b41469b70cb6dd3d99c0aaca
Nick Berveiler <nberveiler@gmail.com>
2012-10-01 20:37:29 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groupcrack::Application.config.secret_token = '40efc6915e6bb1cd31b4b3f2c89ad637fbf0712147fc4b9adea539114cdcdd7c48672b11d3ba09abf0ef05fb48f36f62eef1b690ff682e5e52a14c81d3591b12'
