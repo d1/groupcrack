16148a72b8e0fb9391359c03758eab5f8545bc51
Nick Berveiler <nberveiler@gmail.com>
2012-12-13 00:07:38 -0600

    keep user logged in across all subdomains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Groupcrack::Application.config.session_store :cookie_store, key: '_groupcrack_session'
+Groupcrack::Application.config.session_store :cookie_store, :domain => :all, key: '_groupcrack_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
