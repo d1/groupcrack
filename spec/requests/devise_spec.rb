692f4760be8982ec8641e1c9ca4cb3e0a7c5ae8c
Nick Berveiler <nberveiler@gmail.com>
2013-01-12 10:59:15 -0600

    added a devise integration test to make sure users can sign up

---
+++ spec/requests/devise_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'UserRegistration' do
+  it 'allows a user to register' do
+    visit new_user_registration_path
+    fill_in 'First name', :with => 'New'
+    fill_in 'Last name', :with => 'User'
+    fill_in 'Email', :with => 'newuser@example.com'
+    fill_in 'user_password', :with => 'userpassword'
+    fill_in 'user_password_confirmation', :with => 'userpassword'
+    click_button 'Sign up'
+    page.should have_content 'Welcome'
+  end
+end
